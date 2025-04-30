8d0cc31843ec765cbca4fb401876f4adaa80d58f
suBreathe <3428014070@qq.com>
2025-04-30 12:11:22 +0800

    第三方依赖集成结构

--- add/add.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by suzhe on 25-4-24.
-//
-
-#include "add.h"
-
-int add(int a, int b) {
-    return a + b;
-}
--- sub/sub.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by suzhe on 25-4-25.
-//
-
-#include "sub.h"
-
-int sub(int a, int b) {
-    return a - b;
-}
